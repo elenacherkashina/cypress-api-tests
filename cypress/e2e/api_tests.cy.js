70d71e47e86b5e0464dfa77dc878510a89c1a86e
Elena Cherkashina <Elena.Cherkashina@derivco.com.ua>
2025-05-05 20:28:21 +0300

    Added logging to tests 2

--- cypress/e2e/api_tests.cy.js
+++ cypress/e2e/api_tests.cy.js
@@ -8,7 +8,7 @@ describe('Reqres API Tests', () => {
   
   
     it('GET single user - not found', () => {
-      console.log('🔍 API KEY:', process.env.REQRES_API_KEY);
+      console.log('API KEY:', process.env.REQRES_API_KEY);
       cy.request({
         method: 'GET',
         url: '/api/users/23',
@@ -22,7 +22,7 @@ describe('Reqres API Tests', () => {
     });
 
     it('POST create user', () => {
-      console.log('🔍 API KEY:', process.env.REQRES_API_KEY);
+      console.log('API KEY:', process.env.REQRES_API_KEY);
       cy.request({
         method: 'POST',
         url: '/api/users',
